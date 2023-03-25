4aeb791522a93d6f833f925936ca21135ab41153
dirk945 <115084965+dirk945@users.noreply.github.com>
2023-03-25 21:38:30 +0800

    Update message

--- commandsAndResponses.js
+++ commandsAndResponses.js
@@ -8,5 +8,26 @@ module.exports = {
       description: "Say Hello!",
       category: "General",
       response: "Hello!"
-    }
+    },
+    bye: {
+        description: "Say bye!",
+        category: "General",
+        response: "Bye bye!"
+      },
+    早安: {
+        description: "Speak",
+        category: "General",
+        response: "早安!主人:heart:~"
+      },
+    晚安: {
+        description: "Speak",
+        category: "General",
+        response: "祝你有個好夢!主人:heart:~"
+      },
+
+
+
+
+
+
   };
